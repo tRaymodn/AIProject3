f3f5704e130627f3d57d587aa57cc4a1a379abc7
Tucker Raymond <teraymond@wpi.edu>
2022-12-08 18:38:06 -0500

    Add params to ML algos

--- MachineFailurePredictorPipeline.py
+++ MachineFailurePredictorPipeline.py
@@ -46,7 +46,7 @@ def main():
         #neuralNetwork(split[0], split[1])
         #naiveBayes(split[0], split[1])
         #randomForest(split[0], split[1])
-        supportVectorMachine(split[0], split[1])
+        #supportVectorMachine(split[0], split[1])
         #adaBoost(split[0], split[1])
 def neuralNetwork(training_data, testing_data):
     for row in training_data:  # Convert letters into numerical data
@@ -57,30 +57,67 @@ def neuralNetwork(training_data, testing_data):
             row[2] = 0.3
         else:
             row[2] = 0.2
-    y = []
+
+    trainingFloatList = []  # Turn training data into all numerical floating point values
     for row in training_data:
-        y.append(row[8])
+        newRow = []
+        for att in row:
+            if att == "L":
+                newRow.append(0.5)
+            elif att == "M":
+                newRow.append(0.3)
+            elif att == "H":
+                newRow.append(0.2)
+            else:
+                newRow.append(float(att))
+        trainingFloatList.append(newRow)
+    y_true = []
+    for row in training_data:  # Create an array of ints correctly representing the machine failure for each data point
+        y_true.append(row[8])
 
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-    clf.fit(training_data, y)
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1, max_iter=2000)
+    clf.fit(trainingFloatList, y_true)
 
-    intList = []
-    for row in testing_data:  # Convert all values in testing data into numerical, floating point numbers
+    y_pred = clf.predict(trainingFloatList)
+    print("Prediction: ", y_pred)
+
+    degree = np.array([1, 2, 3])
+    randomState = np.array([9, 8, 10])
+    tol = np.array([0.0001, 0.00011, 0.000010])
+    alpha = np.array([101.46, 101.45, 101.49, 101.6])
+    grid = GridSearchCV(estimator=clf, param_grid={'tol': tol, 'alpha': alpha, 'random_state': randomState})
+    grid.fit(trainingFloatList, y_true)
+    print(grid)
+    # summarize the results of the grid search
+    print("Best grid score: ", grid.best_score_)
+    print("Best estimator for random state: ", grid.best_estimator_.random_state)
+    print("Best estimator for tolerance: ", grid.best_estimator_.tol)
+    print("Best estimator for alpha: ", grid.best_estimator_.alpha)
+
+    testingIntList = []  # Turn training data into all numerical floating point values
+    for row in testing_data:
         newRow = []
         for att in row:
             newRow.append(float(att))
-        intList.append(newRow)
-    print(intList)
-    y_true = []
-    for row in testing_data:  # Create an array of ints correctly representing the machine failure for each data point
-        y_true.append(row[8])
+        testingIntList.append(newRow)
 
-    y_pred = clf.predict(intList)
-    print(y_pred)
+    y_testing_true = []
+    for row in testing_data:  # Create list of true labels values for testing data
+        y_testing_true.append(row[8])
 
-    scores = cross_val_score(clf, intList, y_true, cv=5)
-    print("5-fold cross validation scores: ", scores)
-    print("Average cross validation score: ", sum(scores)/len(scores))
+    clfNew = MLPClassifier(random_state=grid.best_estimator_.random_state,
+                     tol=grid.best_estimator_.tol,
+                     alpha=grid.best_estimator_.alpha)
+    scores = cross_val_score(clfNew, testingIntList, y_testing_true, cv=5)
+    print("5-fold cross validation scores on testing data: ", scores)
+    f1 = sum(scores) / len(scores)
+    print("Average cross validation score on testing data: ", f1)
+
+    output = [['random_state', grid.best_estimator_.random_state],
+              ['tol', grid.best_estimator_.tol],
+              ['alpha', grid.best_estimator_.alpha],
+              [f1]]
+    return output
 
 def supportVectorMachine(trainingData, testingData):
     training_true_y = []
@@ -118,24 +155,41 @@ def supportVectorMachine(trainingData, testingData):
     print(grid)
     # summarize the results of the grid search
     print("Best grid score: ", grid.best_score_)
-    print("Best estimator for coef0: ", grid.best_estimator_.coef0)
-    print("Best estimator for C: ", grid.best_estimator_.C)
+    #print("Best estimator for coef0: ", grid.best_estimator_.coef0)
+    #print("Best estimator for C: ", grid.best_estimator_.C)
     print("Best estimator for degree: ", grid.best_estimator_.degree)
     print("Best estimator for tolerance: ", grid.best_estimator_.tol)
     print("Best estimator for gamma: ", grid.best_estimator_.gamma)
 
-    predFloat = []
-    for value in pred:  # Transform the data from the prediction into floating point values
-        predFloat.append(float(value))
+    testingIntList = []  # Turn training data into all numerical floating point values
+    for row in testingData:
+        newRow = []
+        for att in row:
+            newRow.append(float(att))
+        testingIntList.append(newRow)
 
-    scores = cross_val_score(clf, trainingData, training_true_y, cv=5)  # Compute 5 fold, cross validation with training
-    print("Scores: ", scores)
-    print("Average of F1 scores: ", sum(scores)/len(scores))
+    y_testing_true = []
+    for row in testingData:  # Create list of true labels values for testing data
+        y_testing_true.append(row[8])
+
+    clfNew = svm.SVC(degree=grid.best_estimator_.degree,
+                     tol=grid.best_estimator_.tol,
+                     gamma=grid.best_estimator_.gamma)
+    scores = cross_val_score(clfNew, testingIntList, y_testing_true, cv=5)
+    print("5-fold cross validation scores on testing data: ", scores)
+    f1 = sum(scores) / len(scores)
+    print("Average cross validation score on testing data: ", f1)
+
+    output = [['degree', grid.best_estimator_.degree],
+              ['tolerance', grid.best_estimator_.tol],
+              ['gamma', grid.best_estimator_.gamma],
+              [f1]]
+    return output
 
 
 def randomForest(trainingInput, testingInput):
 
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf = RandomForestClassifier()
 
     for row in trainingInput:
         row[1] = 0
@@ -173,9 +227,35 @@ def randomForest(trainingInput, testingInput):
     print(y_true)
     print(y_pred)
 
-    #print(sklearn.metrics.f1_score(y_true, y_pred, average='weighted'))
-    scores = cross_val_score(clf, trainingInput, y_true, cv=5)  # Compute 5 fold, cross validation with training
-    print("F1 scores with 5-fold validation: ", scores)
+    estimators = np.array([100, 110, 90])
+    maxDepth = np.array([2, 3, 5, 7])
+    maxFeatures = np.array([2, 3, 5, 7])
+    grid = GridSearchCV(estimator=clf, param_grid={'n_estimators': estimators, 'max_depth': maxDepth, 'max_features': maxFeatures})
+    grid.fit(trainingInput, y_true)
+    print(grid)
+    # summarize the results of the grid search
+    print("Best grid score: ", grid.best_score_)
+    print("Best estimator for n_estimators: ", grid.best_estimator_.n_estimators)
+    print("Best estimator for max_depth: ", grid.best_estimator_.max_depth)
+    print("Best estimator for max_features: ", grid.best_estimator_.max_features)
+
+    y_testing_true = []
+    for row in testingInput:  # Create list of true labels values for testing data
+        y_testing_true.append(row[8])
+
+    clfNew = RandomForestClassifier(n_estimators=grid.best_estimator_.n_estimators,
+                                    max_depth=grid.best_estimator_.max_depth,
+                                    max_features=grid.best_estimator_.max_features)
+    scores = cross_val_score(clfNew, testingInput, y_testing_true, cv=5)
+    print("5-fold cross validation scores on testing data: ", scores)
+    f1 = sum(scores) / len(scores)
+    print("Average cross validation score on testing data: ", f1)
+
+    output = [['n_estimators', grid.best_estimator_.n_estimators],
+              ['max_depth', grid.best_estimator_.max_depth],
+              ['max_features', grid.best_estimator_.max_features],
+              [f1]]
+    return output
 
 def adaBoost(trainingData, testingData):
     y_training_true = []
@@ -183,24 +263,48 @@ def adaBoost(trainingData, testingData):
         y_training_true.append(row[8])
     print(y_training_true)
 
-    clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+    clf = AdaBoostClassifier()
     clf.fit(trainingData, y_training_true)
     pred = clf.predict(trainingData)
     print(pred)
 
-    predFloat = []
-    j = 0
-    while j < len(pred):  # Transforms the prediction list into a list of floating point numbers
-        predFloat.append(float(pred[j]))
-        j += 1
+    learningRate = np.array([0.2, 0.5, 1, 2, 3])
+    estimators = np.array([10, 9, 5, 6])
+    randomState = np.array([2, 3, 5, 7, 1, 0])
+    grid = GridSearchCV(estimator=clf,
+                        param_grid={'n_estimators': estimators, 'random_state': randomState, 'learning_rate': learningRate})
+    grid.fit(trainingData, y_training_true)
+    print(grid)
+    # summarize the results of the grid search
+    print("Best grid score: ", grid.best_score_)
+    print("Best estimator for n_estimators: ", grid.best_estimator_.n_estimators)
+    print("Best estimator for random state: ", grid.best_estimator_.random_state)
+    print("Best estimator for learning rate: ", grid.best_estimator_.learning_rate)
+
+    testingIntList = []  # Turn training data into all numerical floating point values
+    for row in testingData:
+        newRow = []
+        for att in row:
+            newRow.append(float(att))
+        testingIntList.append(newRow)
 
     y_testing_true = []
-    for row in testingData:  # Creates a list of all the true labels for the testing data
-        y_testing_true.append(float(row[8]))
-    print(y_testing_true)
+    for row in testingData:  # Create list of true labels values for testing data
+        y_testing_true.append(row[8])
 
-    scores = cross_val_score(clf, trainingData, y_training_true, cv=5)  # Compute 5 fold, cross validation with training
-    print("F1 scores with 5-fold validation: ", scores)
+    clfNew = AdaBoostClassifier(learning_rate=grid.best_estimator_.learning_rate,
+                                random_state=grid.best_estimator_.random_state,
+                                n_estimators=grid.best_estimator_.n_estimators)
+    scores = cross_val_score(clfNew, testingIntList, y_testing_true, cv=5)
+    print("5-fold cross validation scores on testing data: ", scores)
+    f1 = sum(scores) / len(scores)
+    print("Average cross validation score on testing data: ", f1)
+
+    output = [['learning_rate', grid.best_estimator_.learning_rate],
+              ['n_estimators', grid.best_estimator_.n_estimators],
+              ['random_state', grid.best_estimator_.random_state],
+              [f1]]
+    return output
 
 
 def naiveBayes(trainingData, testingData):
@@ -219,21 +323,35 @@ def naiveBayes(trainingData, testingData):
 
     gnb.fit(intList, y_training_true)  # Fit the training data to the labels according to Gaussian Naive Bayes
 
-    testingIntList = []
-    for row in testingData:  # Transform all values in testingData to floating point numbers
+    varSmoothing = np.array([0.00000000010, 0.00000001, 0.000000000001])
+    grid = GridSearchCV(estimator=gnb,
+                        param_grid={'var_smoothing': varSmoothing})
+    grid.fit(intList, y_training_true)
+    print(grid)
+    # summarize the results of the grid search
+    print("Best grid score: ", grid.best_score_)
+    print("Best estimator for variable smoothing: ", grid.best_estimator_.var_smoothing)
+
+    testingIntList = []  # Turn training data into all numerical floating point values
+    for row in testingData:
         newRow = []
         for att in row:
             newRow.append(float(att))
         testingIntList.append(newRow)
 
-    y_pred = gnb.predict(testingIntList)  # Use GNB to make a prediction for the classifiers of testingData
-
     y_testing_true = []
-    for row in testingData:  # Creates a list of all the true labels for the testing data
-        y_testing_true.append(float(row[8]))
+    for row in testingData:  # Create list of true labels values for testing data
+        y_testing_true.append(row[8])
 
-    scores = cross_val_score(gnb, intList, y_training_true, cv=5)  # Compute 5-fold, cross validation with training
-    print("F1 scores with 5-fold validation: ", scores)
+    gnbNew = GaussianNB(var_smoothing=grid.best_estimator_.var_smoothing)
+    scores = cross_val_score(gnbNew, testingIntList, y_testing_true, cv=5)
+    print("5-fold cross validation scores on testing data: ", scores)
+    f1 = sum(scores) / len(scores)
+    print("Average cross validation score on testing data: ", f1)
+
+
+    output = [['var_smoothing', grid.best_estimator_.var_smoothing], [f1]]
+    return output
 
 
 if __name__ == "__main__":
